fac1b971cff7ca2a0189f10ecfdac1fc26b10490
Andrey Belyaev <belyaev.andrey@gmail.com>
2021-08-13 17:01:44 +0300

    Added entities

---
+++ src/main/java/io/jpabuddy/spring/demo/entities/Project.java
@@ -0,0 +1,70 @@
+package io.jpabuddy.spring.demo.entities;
+
+import javax.persistence.*;
+import javax.validation.constraints.PastOrPresent;
+import java.time.LocalDate;
+import java.util.List;
+
+@Table(name = "project")
+@Entity
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @PastOrPresent
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "manager_id", nullable = false)
+    private AppUser manager;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "project_id")
+    private List<Task> tasks;
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public AppUser getManager() {
+        return manager;
+    }
+
+    public void setManager(AppUser manager) {
+        this.manager = manager;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
